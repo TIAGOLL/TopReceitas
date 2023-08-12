f804c4e3da0799b4292f43a9e91e0cb8f902d244
luiz <103447617+luiz0217@users.noreply.github.com>
2023-08-12 18:41:17 -0300

    começo da manipulação de json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import dados from './dados.json'
 
 const App = () => {
+
+    function display() {
+        for (let i = 0; i < dados.services.length; i++) {
+
+            return (
+                <div>
+                    <h1>{dados.services[{ i }].nome}</h1>
+                    <h2>ingredientes</h2>
+                    <p>{dados.services[{ i }].ingredientes}</p>
+                    <h2>modo de preparo</h2>
+                    <p>{dados.services[{ i }].modopreparo}</p>
+                </div>
+            )
+        }
+    }
+
     return (
         <>
-
+            <button onClick={display}>bla</button>
         </>
 
     )
