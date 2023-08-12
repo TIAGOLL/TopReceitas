69e14982583a6e8735e4af118999c33046589af9
Tiago <tiagoepitanga10@gmail.com>
2023-08-12 16:32:50 -0300

    update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './globals.css'
 const App = () => {
     return (
         <>
-            <div className="flex flex-row h-96 w-screen bg-blue-500">
+            <div className="flex flex-row w-screen bg-blue-500">
                 <nav className='flex flex-row'>
                     <ul className='flex flex-row gap-6 m-10'>
                         <li className="p-6">
